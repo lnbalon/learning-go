c04f23b23e51ccad00337b00806464558227d7b4
lnbalon <lnbalon87@gmail.com>
2021-05-12 04:09:13 +0800

    new code

--- 001-hello-world.go
+++ 001-hello-world.go
@@ -4,14 +4,6 @@ import (
 	"fmt"
 )
 
-var temp int16 =42
-
-
-func main() {
-	fmt.Printf("%v %T\n", temp, temp)
-	var temp int = 12
-	var bong float32 
-	bong = 11
-	fmt.Printf("%v %T\n", temp, temp)
-	fmt.Printf("%v %T\n", bong, bong)
+func main(){
+	fmt.Println("hellow")
 }
