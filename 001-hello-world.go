5fb87ee4cac1e7b9bee6059a8f922d79d52fc7fa
lnbalon <lnbalon87@gmail.com>
2021-05-12 03:57:39 +0800

    local variables practice

--- 001-hello-world.go
+++ 001-hello-world.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
-
+var temp int16 =42
 
 
 func main() {
-	var temp int 
-	temp = 11
 	fmt.Printf("%v %T\n", temp, temp)
-
+	var temp int = 12
+	var bong float32 
+	bong = 11
+	fmt.Printf("%v %T\n", temp, temp)
+	fmt.Printf("%v %T\n", bong, bong)
 }
