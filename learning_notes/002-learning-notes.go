ae0f2f50c7ac3a6922f44c9f4d4b7ae504d34ccc
lnbalon <lnbalon87@gmail.com>
2021-05-13 03:40:35 +0800

    more functions lesson

--- learning_notes/002-learning-notes.go
+++ learning_notes/002-learning-notes.go
@@ -6,9 +6,18 @@ import (
 	"math/rand"
 )
 
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x,y string) (string, string) {
+	return y, x
+}
 
 func main() {
 	fmt.Println(rand.Intn(100))
 	fmt.Println(math.Sqrt(7))
 	fmt.Println(math.Pi)
+	fmt.Println(add(23,21))
+	fmt.Println(swap("string", "swap"))
 }
