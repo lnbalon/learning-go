95ebf48f9e1538274efdf3de9372cef17af5558d
lnbalon <lnbalon87@gmail.com>
2021-05-13 03:45:20 +0800

    variables

--- learning_notes/002-learning-notes.go
+++ learning_notes/002-learning-notes.go
@@ -1,32 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"math/rand"
-)
+import "fmt"
 
-func add(x, y int) int {
-	return x + y
-}
-
-func swap(x,y string) (string, string) {
-	return y, x
-}
-
-
-func split(i int) (j, k int) {
-	j = i + 10
-	k = i - 10
-	return
-}
-
+var c, python, java bool
 
 func main() {
-	fmt.Println(rand.Intn(100))
-	fmt.Println(math.Sqrt(7))
-	fmt.Println(math.Pi)
-	fmt.Println(add(23,21))
-	fmt.Println(swap("string", "swap"))
-	fmt.Println(split(100))
+	var i int
+	fmt.Println(i, c, python, java)
 }
