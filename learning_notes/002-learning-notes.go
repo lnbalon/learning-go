dad425500da5cf447887c15c0efc36221cb73192
lnbalon <lnbalon87@gmail.com>
2021-05-13 03:42:15 +0800

    naked return

--- learning_notes/002-learning-notes.go
+++ learning_notes/002-learning-notes.go
@@ -14,10 +14,19 @@ func swap(x,y string) (string, string) {
 	return y, x
 }
 
+
+func split(i int) (j, k int) {
+	j = i + 10
+	k = i - 10
+	return
+}
+
+
 func main() {
 	fmt.Println(rand.Intn(100))
 	fmt.Println(math.Sqrt(7))
 	fmt.Println(math.Pi)
 	fmt.Println(add(23,21))
 	fmt.Println(swap("string", "swap"))
+	fmt.Println(split(100))
 }
