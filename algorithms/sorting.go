53d157d7aac98d6ed7cbdb12bfb18eae16cabaec
lnbalon <lnbalon87@gmail.com>
2021-05-14 18:08:47 +0800

    minor edits

--- algorithms/sorting.go
+++ algorithms/sorting.go
@@ -1,12 +1,8 @@
 package algorithms
 
-import "fmt"
-
 // BubbleSort is the fucking best
 func BubbleSort(x []int) []int {
-	fmt.Println(len(x))
 	end := len(x)
-
 	for end > 0 {
 		for i := 0; i<end-1; i++ {
 			if x[i] > x[i+1] {
